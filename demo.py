4e1eb54a43bd2dda2dd55ca7c552fbd9f82aff3a
TakumaOmiya <omiya.takuma.or1@is.naist.jp>
2022-09-24 17:40:24 +0900

    implement pygame ui

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-import imp
 import time
 import gym
 import dogfight
@@ -6,21 +5,44 @@ import pygame
 
 import numpy as np
 
-pygame.init()
+# pygame.init()
+# screen = pygame.display.set_mode((400, 300))
+# pygame.display.set_caption("keyboard event")
+# import sys
+# while True:
 
-# env = gym.make('CarRacing-v0')
-env = dogfight.DogFight()
+#     screen.fill((0, 0, 0))
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_ESCAPE:
+#                 pygame.quit()
+#                 sys.exit()
+            
+#             else:
+#                 print("押されたキー = " + pygame.key.name(event.key))
+#         pygame.display.update()
+
+
+from check_lander import LunarLander
+env = gym.make('LunarLanderContinuous-v2', render_mode="human")
+
+# env = dogfight.DogFight(render_mode="human")
+# env = LunarLander(render_mode="human")
 num_episodes = 10
+
 for i in range(num_episodes):
     state = env.reset()
+    
     totalReward = 0
 
     for _ in range(100):
         action = np.zeros(2, dtype=np.float32)
         keystate = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            print(event.type)
-            print(pygame.K_BACKQUOTE)
+        # for event in pygame.event.get():
+        #     print(event)
         if keystate[pygame.K_UP]:
             action[1] += 1.0
         if keystate[pygame.K_DOWN]:
@@ -34,11 +56,13 @@ for i in range(num_episodes):
 
         # take a random action
         randomAction = env.action_space.sample()
+        observation = env.step(randomAction)
+        print(observation)
         # observation,reward,done,info = env.step(randomAction) 
-        observation,reward,done,info = env.step(action) 
+        # observation,reward,done,info = env.step(action) 
 
         time.sleep(0.1)
-        totalReward += reward
+        # totalReward += reward
 
     print('Episode', i,', Total reward:', totalReward)
 
--- dogfight.py
+++ dogfight.py
@@ -1,11 +1,30 @@
-from traceback import print_tb
+from typing import TYPE_CHECKING, Optional
+
 import gym
+from gym.error import DependencyNotInstalled
+
 import numpy as np
 import cv2
 
+if TYPE_CHECKING:
+    import pygame
+
+FPS = 50
+
+VIEWPORT_W = 600
+VIEWPORT_H = 600
+
+
 class DogFight(gym.Env):
-    def __init__(self):
-        self.WINDOW_SIZE = 600 #画面サイズの決定
+
+    metadeta = {
+        "render_modes": ["human", "rgb_array"],
+        "render_fps": FPS,
+    }
+
+    def __init__(
+        self,
+        render_mode: Optional[str] = None,):
         self.ACTION_MAP = np.array([[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]) #アクションの用意
         self.GOAL_RANGE = 50 #ゴールの範囲設定
 
@@ -17,14 +36,18 @@ class DogFight(gym.Env):
         self.num_states = 2
         self.observation_space = gym.spaces.Box(np.zeros(self.num_states), np.ones(self.num_states) * np.Inf)
 
-        self.reset()
+        self.screen: pygame.Surface = None
+        self.clock = None
+        self.isopen = True
+
+        self.render_mode = render_mode
 
     def reset(self):
         self.timestep = 0
 
         # ボールとゴールの位置をランダムで初期化
-        self.ball_position = np.random.rand(self.num_states) * float(self.WINDOW_SIZE)
-        self.goal_position = np.random.rand(self.num_states) * float(self.WINDOW_SIZE)
+        self.ball_position = np.random.rand(self.num_states) * float(VIEWPORT_W)
+        self.goal_position = np.random.rand(self.num_states) * float(VIEWPORT_H)
 
         # 状態の作成
         vec = self.ball_position - self.goal_position
@@ -32,6 +55,9 @@ class DogFight(gym.Env):
         observation = np.array([observation])
 
         self.before_distance = np.linalg.norm(vec)
+
+        if self.render_mode == "human":
+            self.render()
 
         return observation
 
@@ -59,15 +85,58 @@ class DogFight(gym.Env):
         return observation, reward, done, {}
 
     def render(self):
+        try:
+            import pygame
+            from pygame import gfxdraw
+        except ImportError:
+            raise DependencyNotInstalled(
+                "pygame is not installed, run `pip install gym[box2d]`"
+            )
+
+        if self.screen is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.screen = pygame.display.set_mode((VIEWPORT_W, VIEWPORT_H))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+        
+        self.surf = pygame.Surface((VIEWPORT_W, VIEWPORT_H))
+
+        pygame.transform.scale(self.surf, (30, 30))
+        pygame.draw.rect(self.surf, (255, 255, 255), self.surf.get_rect())
+
+        # for obj in self.particles:
+        #     obj.ttl -= 0.15
+        #     obj.color1 = (
+        #         int(max(0.2, 0.15 + obj.ttl) * 255),
+        #         int(max(0.2, 0.5 * obj.ttl) * 255),
+        #         int(max(0.2, 0.5 * obj.ttl) * 255),
+        #     )
+        #     obj.color2 = (
+        #         int(max(0.2, 0.15 + obj.ttl) * 255),
+        #         int(max(0.2, 0.5 * obj.ttl) * 255),
+        #         int(max(0.2, 0.5 * obj.ttl) * 255),
+        #     )
+
+        # self._clean_particles(False)
+
         # opencvで描画処理してます
-        img = np.zeros((self.WINDOW_SIZE, self.WINDOW_SIZE, 3)) #画面初期化
-        goal_position = self.goal_position.astype(np.int32)
-        ball_position = self.ball_position.astype(np.int32)
+        # img = np.zeros((VIEWPORT_W, VIEWPORT_H, 3)) #画面初期化
+        # goal_position = self.goal_position.astype(np.int32)
+        # ball_position = self.ball_position.astype(np.int32)
 
-        cv2.circle(img,  tuple(goal_position), 10, (0, 255, 0), thickness=-1) #ゴールの描画
-        cv2.circle(img, tuple(goal_position), self.GOAL_RANGE, color=(0,255,0), thickness=5) #ゴールの範囲の描画
+        # cv2.circle(img,  tuple(goal_position), 10, (0, 255, 0), thickness=-1) #ゴールの描画
+        # cv2.circle(img, tuple(goal_position), self.GOAL_RANGE, color=(0,255,0), thickness=5) #ゴールの範囲の描画
 
-        cv2.circle(img,  tuple(ball_position), 10, (0, 0, 255), thickness=-1) #ボールの描画
+        # cv2.circle(img,  tuple(ball_position), 10, (0, 0, 255), thickness=-1) #ボールの描画
 
-        cv2.imshow('image', img)
-        cv2.waitKey(1)
+        # cv2.imshow('image', img)
+        # cv2.waitKey(1)
+
+    def close(self):
+        if self.screen is not None:
+            import pygame
+
+            pygame.display.quit()
+            pygame.quit()
+            self.isopen = False
